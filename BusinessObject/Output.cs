cee1f13c859659ba330317f9cd9940fcebd428e3
Khai Nguyen <155701970+ntkhaii225@users.noreply.github.com>
2024-11-15 01:37:58 +0700

    User window

---
+++ BusinessObject/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject;
+
+public partial class Output
+{
+    public int OutputId { get; set; }
+
+    public DateOnly OutputDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+	public virtual ICollection<OutputInfo> OutputInfos { get; set; } = new List<OutputInfo>();
+}
---
+++ QuanLiKho/CustomerWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using BusinessObject;
+using DataAccessLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLiKho
+{
+    /// <summary>
+    /// Interaction logic for CustomerWindow.xaml
+    /// </summary>
+    public partial class CustomerWindow : Window
+    {
+        private CustomerRepository _customerRepository = new();
+        public CustomerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void FillDataGrid(List<Customer> arr)
+        {
+            CustomerDataGrid.ItemsSource = null;
+            CustomerDataGrid.ItemsSource = arr;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            CustomerDataGrid.ItemsSource = _customerRepository.GetAllCustomers();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+
+}
