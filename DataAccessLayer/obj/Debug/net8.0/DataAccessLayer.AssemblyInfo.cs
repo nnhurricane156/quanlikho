7fe319389c3a5af49e46025dc57628c88b6e5771
lken1514 <kienl8890@gmail.com>
2024-11-15 01:54:32 +0700

    Crud unit + suppliers

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382106e49af3e5b0642bafe11fd97027177e9456")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QuanLiKho/UnitWindow.xaml.cs
+++ QuanLiKho/UnitWindow.xaml.cs
@@ -1,65 +1,98 @@
-﻿using DataAccessLayer.Repository;
+﻿using BusinessObject;
+using DataAccessLayer.Repository;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace QuanLiKho
 {
-    /// <summary>
-    /// Interaction logic for UnitWindow.xaml
-    /// </summary>
     public partial class UnitWindow : Window
     {
-		private readonly IUnitRepository unitRepository;
+        private readonly IUnitRepository unitRepository;
 
-		public UnitWindow()
+        public ObservableCollection<Unit> UnitList { get; set; }
+
+        public Unit SelectedUnit { get; set; }
+
+        public UnitWindow()
         {
-			unitRepository = new UnitRepository();
+            unitRepository = new UnitRepository();
             InitializeComponent();
-			LoadUnitList();
-
-		}
-		public void LoadUnitList()
-		{
-			var list = unitRepository.GetAllUnits();			 
-			this.DataContext = new { List = list } ;
-		}
-
-		private void Window_Loaded(object sender, RoutedEventArgs e)
-		{
-			LoadUnitList();		
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-
+            LoadUnitList();
         }
 
-		private void Button_Click_1(object sender, RoutedEventArgs e)
-		{
+        public void LoadUnitList()
+        {
+            UnitList = new ObservableCollection<Unit>(unitRepository.GetAllUnits());
+            this.DataContext = this;
+        }
 
-		}
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var unitName = UnitNameTextBox.Text;
+            if (!string.IsNullOrEmpty(unitName))
+            {
+                Unit newUnit = new Unit { UnitName = unitName };
+                unitRepository.AddUnit(newUnit);
+                UnitList.Add(newUnit);
+                UnitNameTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Unit name cannot be empty.");
+            }
+        }
 
-		private void Button_Click_2(object sender, RoutedEventArgs e)
-		{
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUnit = SelectedUnit;
+            if (selectedUnit != null)
+            {
+                selectedUnit.UnitName = UnitNameTextBox.Text;
 
-		}
+                unitRepository.UpdateUnit(selectedUnit);
 
-		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-		{
+                var unitIndex = UnitList.IndexOf(selectedUnit);
+                if (unitIndex >= 0)
+                {
+                    UnitList[unitIndex] = selectedUnit;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a unit to edit.");
+            }
+        }
 
-		}
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUnit = SelectedUnit;
+            if (selectedUnit != null)
+            {
+                unitRepository.DeleteUnit(selectedUnit.UnitId);
+                UnitList.Remove(selectedUnit);
+            }
+            else
+            {
+                MessageBox.Show("Please select a unit to delete.");
+            }
+        }
 
-		
-	}
+        private void UnitListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedUnit = (Unit)UnitListView.SelectedItem;
+            if (selectedUnit != null)
+            {
+                SelectedUnit = selectedUnit;
+                UnitNameTextBox.Text = selectedUnit.UnitName;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadUnitList();
+        }
+    }
 }
